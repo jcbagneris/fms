01f2679875424ae6423c7cdf4cadf2c0ba0b1b01
Jean-Charles Bagneris <jcbagneris@gmail.com>
2009-01-15 13:53:08 +0100

    Moved option parser in fms/__init__.py

--- fms/__init__.py
+++ fms/__init__.py
@@ -4,6 +4,9 @@
 FMS core module.
 """
 
+import sys
+import optparse
+
 VERSION = '0.1.2'
 
 def get_full_version():
@@ -19,3 +22,20 @@ def get_full_version():
     if git_commit:
         version = "%s-%s" % (version, git_commit)
     return version
+
+def set_parser():
+    """
+    Create command line arguments and options parser
+    """
+    optp = optparse.OptionParser(
+        description='run a Financial Market Simulator simulation',
+        prog='%s' % sys.argv[0],
+        usage="%prog [options] [command] simulationconffile",)
+    optp.add_option('--version', action='store_true', 
+        help="output FMS version and exit")
+    optp.add_option('-v', '--verbose', action='store_true', 
+        help="set logging level to 'info', overrided by --loglevel")
+    optp.add_option('-L', '--loglevel', metavar='LEVEL', dest='loglevel',
+        help="set logging level to LEVEL: debug, info, warning, error, critical")
+    return optp
+
